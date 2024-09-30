f5ad95b11b2c5280e4c6f8f2cdc5b1b296806a00
saheedoluwatosin <saheedoluwatosin4@gmail.com>
2024-09-30 18:11:24 +0100

    updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,34 @@
+const { Admin } = require("../Model/user")
 
-const validtokenadmin =(request,response)=>
+const validtokenadmin = async (request,response,next)=>
 {
+
+    try {
+        const token = request.header("Authorization")
+        if(!token){
+           return response.status(404).json({
+               message:"Access Denied"
+           })
+        }
+        const tkk = token.split(" ")
+        const token1 = tkk[1]
+   
+        const decoded = jwt.verify(token1,process.env.ACCESS_TOKEN)
+        const admin = await Admin.findOne({_id:decoded.alreadyadmin._id})
+   
+        if(!decoded){
+           return response.status(401).json({
+               message:"Invalid Login"
+           })
+        }
+        request.admin = admin 
+        next()
+    } catch (error) {
+        
+    }
     
 }
 
 
 
+module.exports = validtokenadmin
